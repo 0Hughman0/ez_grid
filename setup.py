f9ede773fc3efa054292112ae5e1abac49c97bed
0Hughman0 <rammers2@hotmail.co.uk>
2017-02-05 17:32:36 +0000

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='ez_grid',
       packages=['ez_grid'],
-      version='0.1.1',
+      version='0.1.2',
       description="""A lightweight convenient grid class
 
 ez_grid provides the class Grid that allows for the construction and manipulation of 2D gridded structures.
@@ -15,7 +15,7 @@ No need to install the whole of Pandas for this simple task!""",
       author_email="rammers2@hotmail.co.uk",
       license="MIT",
       url='https://github.com/0Hughman0/ez_grid',
-      download_url='https://github.com/0Hughman0/ez_grid/tarball/0.1.1',
+      download_url='https://github.com/0Hughman0/ez_grid/tarball/0.1.2',
       keywords=["grid", "config", "lightweight", "no dependencies", "configuration", "csv", "config"],
       classifiers=[]
      )
